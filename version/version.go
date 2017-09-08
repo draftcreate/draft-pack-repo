9c9a70e8914efbf1f1d94f5043b796a81fd637de
Matthew Fisher <matt.fisher@microsoft.com>
2017-09-08 11:50:24 -0700

    bump version to v0.1.0

--- version/version.go
+++ version/version.go
@@ -27,7 +27,7 @@ var (
 	// If it is a development build, the release name is called "canary".
 	//
 	// This number is incremented automatically via build.sh.
-	Release = "canary"
+	Release = "v0.1.0"
 
 	// BuildMetadata is extra build time data
 	BuildMetadata = ""
